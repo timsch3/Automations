f6bff585354aa9f154da60bb9bde483ba9394221
Tim Sch <timsch3@gmail.com>
2021-07-17 15:15:01 +0200

    added css and js linking in index.html

---
+++ Automations.psm1
@@ -0,0 +1,25 @@
+function mkhtml($dir) {
+    md $dir/assets/html
+    md $dir/assets/css
+    ni $dir/assets/css/style.css
+    md $dir/assets/js
+    ni $dir/assets/js/main.js
+    md $dir/assets/fonts
+    md $dir/assets/img
+    ni $dir/index.html
+    Add-Content $dir/index.html '<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Freaky Art</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    
+    <script src="assets/js/main.js"></script>
+</body>
+</html>'
+}
